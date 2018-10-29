5f99e14399138880cebc8a9a68b677966a17bf10
albertluck88 <creativeprogramer88@gmail.com>
2018-10-29 16:30:03 +0700

    data

---
+++ build/precache-manifest.90cc117c80ecbccb6ebda58be265763e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "329f334332070f2ee782",
+    "url": "/static/js/main.329f3343.chunk.js"
+  },
+  {
+    "revision": "383c046ead63ed6beb8a",
+    "url": "/static/js/1.383c046e.chunk.js"
+  },
+  {
+    "revision": "329f334332070f2ee782",
+    "url": "/static/css/main.63f6c645.chunk.css"
+  },
+  {
+    "revision": "1f9a8724d7d34f118e21cb40cf2edfd3",
+    "url": "/index.html"
+  }
+];
